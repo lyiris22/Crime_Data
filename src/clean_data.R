2b932959baa47f4478a0675c3ba3c1ba68f28177
Ayla Pearson <aylapearson23@gmail.com>
2019-01-16 10:46:57 -0800

    lat long conversion

--- src/clean_data.R
+++ src/clean_data.R
@@ -30,38 +30,38 @@ crime <- crime %>%
 
 crime_cleaned <- crime %>% 
   select('city','year','violent_per_100k','homs_per_100k','rape_per_100k','rob_per_100k','agg_ass_per_100k','total_pop') 
-  
-  #write_csv('../data/clean_data.csv')
 
-crime_cleaned
-
-
-# dont read it out to csv here, read out after
 
 # ayla's work
 #google will only let use do 2500 entries a day and our data is 2829 values
 top_half_crime <- crime_cleaned[1:1500, ]
-top_half_crime
 
-top_half_crime <- top_half_crime %>% 
-  mutate_geocode(city, output = c("latlon"), source = "dsk")    # didn't see the cleaned city name used department... will use tomorrow but maxed out values
+#top_half_crime <- top_half_crime %>% 
+#  mutate_geocode(city, output = c("latlon"), source = "dsk")   #commented out because you can only do 2500 per day so only run when required 
 
-top_half_crime
-
-write_csv(top_half_crime, 'data/top_half_lat_long.csv' )
+# writes top half of converted lat/longs
+write_csv(top_half_crime, 'data/top_half_lat_long.csv' )  
 
 
-# there are over 50 warnings that need to be clean up manually or investigate why those are working specifically 
-# they place NA where it gives a warning, there are only about 13 cities 
-
+# splits data into chunks for lat/long from google maps
 bottom_half_crime <- crime_cleaned[1501:2829,]
 bottom_half_crime
 
-bottom_half_crime<- bottom_half_crime %>% 
-  mutate_geocode(city, output = c("latlon"), source = "dsk")
+#bottom_half_crime<- bottom_half_crime %>% 
+#  mutate_geocode(city, output = c("latlon"), source = "dsk")   #commented out because you can only do 2500 per day so only run when required
 
+# writes bottom half of converted lat/longs
 write_csv(bottom_half_crime, 'data/bottom_half_lat_long.csv' )
 
-#crime <- inner_join(top_half_crime, bottom_half_crime)   #not sure if this is the right join try once data written
+
+
+crime_bottom = read_csv('data/bottom_half_lat_long.csv')
+crime_top = read.csv('data/top_half_lat_long.csv')
+
+
+# bind top and bottom to create final cleaned data
+crime_latlong <- bind_rows(crime_top, crime_bottom)  
+
+write_csv(crime_latlong, 'data/crime_lat_long.csv' )
 
 ###
